c9e6db5e4f11fe7e2e606a4a6f850d6ed0fd5a33
Felipe-Tommaselli <f.tommaselli@usp.br>
2024-05-24 20:05:32 -0500

    organizing repo

--- gym_env/envs/pygame_2d.py
+++ gym_env/envs/pygame_2d.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 
+from gym_env.envs.car import Car
+
 screen_width = 960 # 1500 
 screen_height = 540 # 800
 w_ini = screen_height//2 + 150
@@ -8,113 +10,6 @@ h_ini = screen_width//2 - 35
 #TODO: ADD CHECKPOINTS
 check_point = ((1200, 660), (1250, 120), (190, 200), (1030, 270), (250, 475), (650, 690))
 
-class Car:
-    def __init__(self, car_file, map_file, pos):
-        self.surface = pygame.image.load(car_file)
-        self.map = pygame.image.load(map_file)
-        self.surface = pygame.transform.scale(self.surface, (60, 60))
-        self.rotate_surface = self.surface
-        self.pos = pos
-        self.angle = 0
-        self.speed = 0
-        self.center = [self.pos[0] + 30, self.pos[1] + 30]
-        self.radars = []
-        self.radars_for_draw = []
-        self.is_alive = True
-        self.current_check = 0
-        self.prev_distance = 0
-        self.cur_distance = 0
-        self.goal = False
-        self.check_flag = False
-        self.distance = 0
-        self.time_spent = 0
-        for d in range(-45, 46, 45):
-            self.check_radar(d)
-
-        for d in range(-45, 46, 45):
-            self.check_radar_for_draw(d)
-
-
-    def draw(self, screen):
-        screen.blit(self.rotate_surface, self.pos)
-
-    def draw_collision(self, screen):
-        for i in range(4):
-            x = int(self.four_points[i][0])
-            y = int(self.four_points[i][1])
-            pygame.draw.circle(screen, (255, 255, 255), (x, y), 5)
-
-    def draw_radar(self, screen):
-        for r in self.radars_for_draw:
-            pos, dist = r
-            pygame.draw.line(screen, (0, 255, 0), self.center, pos, 1)
-            pygame.draw.circle(screen, (0, 255, 0), pos, 5)
-
-    def check_collision(self):
-        self.is_alive = True
-        for p in self.four_points:
-            if self.map.get_at((int(p[0]), int(p[1]))) == (255, 255, 255, 255):
-                self.is_alive = False
-                break
-
-    def check_radar(self, degree):
-        len = 0
-        x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * len)
-        y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * len)
-
-        while not self.map.get_at((x, y)) == (255, 255, 255, 255) and len < 300:
-            len = len + 1
-            x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * len)
-            y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * len)
-
-        dist = int(math.sqrt(math.pow(x - self.center[0], 2) + math.pow(y - self.center[1], 2)))
-        self.radars.append([(x, y), dist])
-
-
-    def check_radar_for_draw(self, degree):
-        len = 0
-        x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * len)
-        y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * len)
-
-        while not self.map.get_at((x, y)) == (255, 255, 255, 255) and len < 300:
-            len = len + 1
-            x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * len)
-            y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * len)
-
-        dist = int(math.sqrt(math.pow(x - self.center[0], 2) + math.pow(y - self.center[1], 2)))
-        self.radars_for_draw.append([(x, y), dist])
-
-    def update(self):
-        #check speed
-        self.speed -= 1.0
-        if self.speed > 10:
-            self.speed = 10
-        if self.speed < 1:
-            self.speed = 1
-
-        #check position
-        self.rotate_surface = rot_center(self.surface, self.angle)
-        self.pos[0] += math.cos(math.radians(360 - self.angle)) * self.speed
-        if self.pos[0] < 20:
-            self.pos[0] = 20
-        elif self.pos[0] > screen_width - 120:
-            self.pos[0] = screen_width - 120
-        self.distance += self.speed
-        self.time_spent += 1
-        self.pos[1] += math.sin(math.radians(360 - self.angle)) * self.speed
-        if self.pos[1] < 20:
-            self.pos[1] = 20
-        elif self.pos[1] > screen_height - 120:
-            self.pos[1] = screen_height - 120
-
-        # caculate 4 collision points
-        self.center = [int(self.pos[0]) + 30, int(self.pos[1]) + 30]
-        len = 20
-        left_top = [self.center[0] + math.cos(math.radians(360 - (self.angle + 30))) * len, self.center[1] + math.sin(math.radians(360 - (self.angle + 30))) * len]
-        right_top = [self.center[0] + math.cos(math.radians(360 - (self.angle + 150))) * len, self.center[1] + math.sin(math.radians(360 - (self.angle + 150))) * len]
-        left_bottom = [self.center[0] + math.cos(math.radians(360 - (self.angle + 210))) * len, self.center[1] + math.sin(math.radians(360 - (self.angle + 210))) * len]
-        right_bottom = [self.center[0] + math.cos(math.radians(360 - (self.angle + 330))) * len, self.center[1] + math.sin(math.radians(360 - (self.angle + 330))) * len]
-        self.four_points = [left_top, right_top, left_bottom, right_bottom]
 
 class PyGame2D:
     def __init__(self):
@@ -201,10 +96,4 @@ class PyGame2D:
         self.clock.tick(self.game_speed)
 
 
-def rot_center(image, angle):
-    orig_rect = image.get_rect()
-    rot_image = pygame.transform.rotate(image, angle)
-    rot_rect = orig_rect.copy()
-    rot_rect.center = rot_image.get_rect().center
-    rot_image = rot_image.subsurface(rot_rect).copy()
-    return rot_image
+
---
+++ simulation.py
@@ -0,0 +1,83 @@
+import sys
+import numpy as np
+import math
+import random
+
+
+import csv
+
+
+import gymnasium as gym
+import gym_env
+
+
+def simulate(env, q_table, csv_file_path, MAX_EPISODES=9999, MAX_TRY=1000, learning_rate=0.3, gamma=0.6, epsilon=0.6, epsilon_decay=0.999):
+    epsilon, epsilon_decay
+    for episode in range(MAX_EPISODES):
+
+        # Init environment
+        state = env.reset(seed=episode)[0]
+        total_reward = 0
+
+        # count random actions
+        crandom  = 0
+        clearned = 0 
+
+        # save infos for analysis
+        totalreward_list = list()
+        reward_list = list()
+        eps_list = list()
+        actions_list = [0, 0, 0, 0]
+        with open(csv_file_path, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            # AI tries up to MAX_TRY times
+            for t in range(MAX_TRY):
+                
+                # In the beginning, do random action to learn
+                if random.uniform(0, 1) < epsilon:
+                    crandom += 1
+                    action = env.action_space.sample()
+                else:
+                    clearned += 1
+                    action = np.argmax(q_table[state])
+
+                actions_list.pop(0)
+                actions_list.append(action)
+
+                # if actions list full of same actions, change last action randomly
+                actions_list[-1] = random.randint(0, 2) if actions_list.count(actions_list[-1]) >= 3 else actions_list[-1]
+                action = actions_list[-1]
+
+                # Do action and get result
+                next_state, reward, done, truncated, _ = env.step(action)
+                #TODO: Fix this (negative reward)
+                total_reward += reward
+
+                # Get correspond q value from state, action pair
+                q_value = q_table[state][action]
+                best_q = np.max(q_table[next_state])
+
+                #TODO: Debug this
+                # Q(state, action) <- (1 - a)Q(state, action) + a(reward + rmaxQ(next state, all actions))
+                q_table[state][action] = (1 - learning_rate) * q_value + learning_rate * (reward + gamma * best_q)
+
+                # Set up for the next iteration
+                state = next_state
+
+                # Draw games
+                env.render()
+
+                # save infos
+                totalreward_list.append(total_reward)
+                reward_list.append(reward)
+                eps_list.append(t)
+
+                # When episode is done, print reward
+                if done or t >= MAX_TRY - 1:
+                    writer.writerow([episode, t, reward, total_reward])
+                    print(f"Ep: {episode} ... Time Steps: {t} ... Total reward = {total_reward:.2f} ... randomness: {crandom/(crandom + clearned):.2f}")
+                    break
+
+        # exploring rate decay
+        if epsilon >= 0.005:
+            epsilon *= epsilon_decay
